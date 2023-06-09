8fc8f5c27c8993b0bb1d85cd59d8c865dbc32dbf
Rutuja412 <rutujakadam4545@gmail.com>
2023-06-09 22:10:26 +0530

    [Rutuja]:Check Employee Daily Wage for Part time

--- Employee-Management/Employee.cs
+++ Employee-Management/Employee.cs
@@ -8,13 +8,19 @@ namespace Employee_Management
     {
         public static void EmployeeAttendance()
         {
-            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
-            int empHrs = 0 ;
+            int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_PART_TIME)
+            {
+                Console.WriteLine("Employee is Partially Present");
+                empHrs = 4;
+            }
+            else if (empCheck == IS_FULL_TIME)
             {
                 Console.WriteLine("Employee is Present");
                 empHrs = 8;
